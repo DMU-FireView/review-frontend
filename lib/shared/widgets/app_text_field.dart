55c43e6bafbcff539421d15cf976ee43463adca5
b0e2 <beenjeong02@gmail.com>
2026-04-23 12:00:00 +0900

    feat: add shared UI components

--- lib/shared/widgets/app_text_field.dart
+++ lib/shared/widgets/app_text_field.dart
@@ -10,6 +10,14 @@ class AppTextField extends StatelessWidget {
     this.obscureText = false,
     this.validator,
     this.onChanged,
+    this.onFieldSubmitted,
+    this.autovalidateMode,
+    this.enabled,
+    this.prefixIcon,
+    this.suffixIcon,
+    this.maxLines = 1,
+    this.autofocus = false,
+    this.textCapitalization = TextCapitalization.none,
     super.key,
   });
 
@@ -21,6 +29,14 @@ class AppTextField extends StatelessWidget {
   final bool obscureText;
   final FormFieldValidator<String>? validator;
   final ValueChanged<String>? onChanged;
+  final ValueChanged<String>? onFieldSubmitted;
+  final AutovalidateMode? autovalidateMode;
+  final bool? enabled;
+  final Widget? prefixIcon;
+  final Widget? suffixIcon;
+  final int? maxLines;
+  final bool autofocus;
+  final TextCapitalization textCapitalization;
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +47,18 @@ class AppTextField extends StatelessWidget {
       obscureText: obscureText,
       validator: validator,
       onChanged: onChanged,
-      decoration: InputDecoration(labelText: labelText, hintText: hintText),
+      onFieldSubmitted: onFieldSubmitted,
+      autovalidateMode: autovalidateMode,
+      enabled: enabled,
+      maxLines: obscureText ? 1 : maxLines,
+      autofocus: autofocus,
+      textCapitalization: textCapitalization,
+      decoration: InputDecoration(
+        labelText: labelText,
+        hintText: hintText,
+        prefixIcon: prefixIcon,
+        suffixIcon: suffixIcon,
+      ),
     );
   }
 }
