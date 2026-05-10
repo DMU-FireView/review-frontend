525327eed9c252b36b9a2d9e6cac17195783089c
b0e2 <beenjeong02@gmail.com>
2026-05-11 05:31:09 +0900

    feat: connect server api clients

--- lib/features/auth/domain/usecases/signup_use_case.dart
+++ lib/features/auth/domain/usecases/signup_use_case.dart
@@ -1,5 +1,4 @@
 import 'package:re_view_front/core/result/result.dart';
-import 'package:re_view_front/features/auth/domain/entities/auth_user.dart';
 import 'package:re_view_front/features/auth/domain/entities/oauth_provider.dart';
 import 'package:re_view_front/features/auth/domain/repositories/auth_repository.dart';
 
@@ -8,7 +7,7 @@ class SignupUseCase {
 
   final AuthRepository _repository;
 
-  Future<Result<AuthUser>> call({
+  Future<Result<void>> call({
     required String name,
     required String email,
     required String password,
