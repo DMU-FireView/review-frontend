07a5821b93584d69d1ccf77a8842a4d81bcdd226
b0e2 <beenjeong02@gmail.com>
2026-05-25 23:15:31 +0900

    feat: add productPath and reviewFeedbackBasePath to AppConfig

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -3,6 +3,8 @@ class AppConfig {
     required this.apiBaseUrl,
     required this.homeDashboardPath,
     required this.searchPath,
+    required this.productPath,
+    required this.reviewFeedbackBasePath,
     required this.loginPath,
     required this.signupPath,
     required this.naverOAuthPath,
@@ -26,6 +28,14 @@ class AppConfig {
       searchPath: String.fromEnvironment(
         'SEARCH_PATH',
         defaultValue: '/api/products',
+      ),
+      productPath: String.fromEnvironment(
+        'PRODUCT_PATH',
+        defaultValue: '/api/products',
+      ),
+      reviewFeedbackBasePath: String.fromEnvironment(
+        'REVIEW_FEEDBACK_BASE_PATH',
+        defaultValue: '/api/reviews',
       ),
       loginPath: String.fromEnvironment(
         'LOGIN_PATH',
@@ -59,6 +69,8 @@ class AppConfig {
   final String apiBaseUrl;
   final String homeDashboardPath;
   final String searchPath;
+  final String productPath;
+  final String reviewFeedbackBasePath;
   final String loginPath;
   final String signupPath;
   final String naverOAuthPath;
