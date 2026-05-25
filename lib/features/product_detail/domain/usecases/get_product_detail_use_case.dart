5822a6079526401f42229e0ab35892a82f0fb480
b0e2 <beenjeong02@gmail.com>
2026-05-25 23:17:33 +0900

    feat: align product detail domain layer with API spec

--- lib/features/product_detail/domain/usecases/get_product_detail_use_case.dart
+++ lib/features/product_detail/domain/usecases/get_product_detail_use_case.dart
@@ -1,8 +1,6 @@
 import 'package:re_view_front/core/result/result.dart';
 import 'package:re_view_front/features/product_detail/domain/entities/product_detail.dart';
 import 'package:re_view_front/features/product_detail/domain/entities/product_review.dart';
-import 'package:re_view_front/features/product_detail/domain/entities/review_insight.dart';
-import 'package:re_view_front/features/product_detail/domain/entities/similar_product.dart';
 import 'package:re_view_front/features/product_detail/domain/repositories/product_detail_repository.dart';
 
 class GetProductDetailUseCase {
@@ -19,27 +17,15 @@ class GetProductReviewsUseCase {
 
   final ProductDetailRepository _repository;
 
-  Future<Result<List<ProductReview>>> call(
-    int productId, {
-    int page = 1,
-    int pageSize = 20,
-  }) => _repository.getProductReviews(productId, page: page, pageSize: pageSize);
+  Future<Result<List<ProductReview>>> call(int productId) =>
+      _repository.getProductReviews(productId);
 }
 
-class GetReviewInsightUseCase {
-  const GetReviewInsightUseCase(this._repository);
+class SubmitReviewFeedbackUseCase {
+  const SubmitReviewFeedbackUseCase(this._repository);
 
   final ProductDetailRepository _repository;
 
-  Future<Result<ReviewInsight>> call(int productId) =>
-      _repository.getReviewInsight(productId);
-}
-
-class GetSimilarProductsUseCase {
-  const GetSimilarProductsUseCase(this._repository);
-
-  final ProductDetailRepository _repository;
-
-  Future<Result<List<SimilarProduct>>> call(int productId) =>
-      _repository.getSimilarProducts(productId);
+  Future<Result<void>> call(int reviewId, String feedbackType) =>
+      _repository.submitReviewFeedback(reviewId, feedbackType);
 }
