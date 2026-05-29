6db2d8f11623470b3368a56e6ced8a00885d3589
b0e2 <beenjeong02@gmail.com>
2026-05-29 21:13:56 +0900

    fix: reload dashboard data on login state change to clear personalized data on logout

--- lib/features/home/presentation/view_models/home_dashboard_view_model.dart
+++ lib/features/home/presentation/view_models/home_dashboard_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:re_view_front/core/providers/core_providers.dart';
 import 'package:re_view_front/features/home/domain/usecases/get_home_dashboard_use_case.dart';
 import 'package:re_view_front/features/home/presentation/providers/home_providers.dart';
 import 'package:re_view_front/features/home/presentation/view_models/home_dashboard_state.dart';
@@ -9,6 +10,7 @@ class HomeDashboardViewModel extends Notifier<HomeDashboardState> {
   @override
   HomeDashboardState build() {
     _getHomeDashboardUseCase = ref.watch(getHomeDashboardUseCaseProvider);
+    ref.watch(isLoggedInProvider); // 로그인/로그아웃 시 provider 재빌드 → 데이터 재fetch
     Future.microtask(load);
     return const HomeDashboardLoading();
   }
